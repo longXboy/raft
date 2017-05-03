3c0423d3b3cf4eca4cc1cd1718224adf558da2cd
longxboy <longxia.cao@daocloud.io>
2017-05-03 09:25:05 +0800

    add timer

--- raft.go
+++ raft.go
@@ -1,5 +1,16 @@
 package raft
 
+import (
+	"log"
+	"math/rand"
+	"net"
+	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
 type StateType uint16
 
 const (
@@ -34,6 +45,46 @@ type Raft struct {
 	matchIndex []uint64
 }
 
+func (raft *Raft) Vote(ctx context.Context, req *VoteRequest) (*VoteReply, error) {
+	return &VoteReply{}, nil
+}
+
+func (raft *Raft) Append(ctx context.Context, req *AppendRequest) (*AppendReply, error) {
+	return &AppendReply{}, nil
+}
+
 func NewRaft(id uint64, peers []uint64) *Raft {
 	return &Raft{ID: id, peers: peers}
+}
+
+func (raft *Raft) MainLoop() {
+	var lastHeartBeat time.Time
+	lastTimer := time.Now()
+	n := rand.Intn(150)
+	timer := time.NewTimer(time.Duration(int64(150+n) * int64(time.Millisecond)))
+	defer timer.Stop()
+	for {
+		select {
+		case <-timer.C:
+			if lastHeartBeat.After(lastTimer) {
+				next := (time.Millisecond * 150) - time.Now().Sub(lastHeartBeat)
+				timer.Reset(next)
+			}
+		}
+	}
+}
+
+func (raft *Raft) Start() {
+	go raft.MainLoop()
+
+	lis, err := net.Listen("tcp", ":7070")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	RegisterRaftRPCServer(s, raft)
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
